93de84c72189ee343e9a9684abc48bcf8defb2c9
AravArora05 <aravarora05@gmail.com>
2024-06-13 17:07:52 -0400

    Setup JSON Server as Vercel serverless function

--- api/server.js
+++ api/server.js
@@ -1,8 +1,3 @@
-// api/server.js
-module.exports = (req, res) => {
-  res.status(200).send('Hello World from Vercel!');
-};
-
 const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('./db.json'); // This path assumes db.json is in the same directory as server.js
