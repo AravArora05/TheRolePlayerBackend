f8f1cabd959594ab58cb513fa3437c7b62868048
AravArora05 <aravarora05@gmail.com>
2024-06-13 23:25:12 -0400

    Prepare for deployment to Vercel

--- api/server.js
+++ api/server.js
@@ -1,9 +1,3 @@
-const jsonServer = require('json-server');
-const server = jsonServer.create();
-const router = jsonServer.router('db.json');  // Make sure the path to 'db.json' is correct
-const middlewares = jsonServer.defaults();
-
-server.use(middlewares);
-server.use(router);
-
-module.exports = server;
+/**
+ * No more
+ */
